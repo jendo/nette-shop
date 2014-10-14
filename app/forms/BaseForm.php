12531ab58af5c56cab5e732e2415e149896c2dbc
Michal Jenis <michal.jenis@idnes.cz>
2014-10-14 12:43:27 +0200

    Add BaseForm

--- app/forms/BaseForm.php
+++ app/forms/BaseForm.php
@@ -4,15 +4,24 @@ namespace Forms;
 
 use Nette;
 
+/**
+ * Spolocny predok pre vsetky formulare, kotre maju vlastny template a vlastne callbacky na signaly
+ * Formular musi dedit od Nette\Application\UI\Form (formular priamo presenteroch) a nie od Nette\Forms\Form ak chceme mat platne valstne callbacky
+ */
 abstract class BaseForm extends Nette\Application\UI\Form
 {
+
 	public function __construct(Nette\ComponentModel\IContainer $parent = NULL, $name = NULL)
 	{
 		parent::__construct($parent, $name);
 		$this->init();
+		$this->onValidate[] = array($this, 'formValidate');
+		$this->onSubmit[] = array($this, 'formSubmitted');
+		$this->onSuccess[] = array($this, 'processForm');
+		$this->onError[] = array($this, 'processError');
 	}
-	
-	/**
+
+		/**
 	 * Gets template file name
 	 *
 	 * @return string
@@ -26,12 +35,32 @@ abstract class BaseForm extends Nette\Application\UI\Form
 		$class = strtolower(substr($class, 0, 1)) . substr($class, 1);
 		return $dir . $class . '.latte';
 	}
-	
+
 	/**
 	 * Creates form
 	 *
 	 * @return void
 	 */
 	abstract protected function init();
-	
+
+	/**
+	 * Occurs when the form is validated
+	 */
+	abstract public function formValidate($form);
+
+	/**
+	 * Occurs when the form is submitted
+	 */
+	abstract public function formSubmitted($form);
+
+	/**
+	 * Occurs when the form is submitted and successfully validated
+	 */
+	abstract public function processForm($form);
+
+	/**
+	 * Occurs when the form is submitted and is not valid
+	 */
+	abstract public function processError($form);
+
 }
