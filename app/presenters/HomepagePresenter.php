1755dd938fc69c94448bcee11761624b018fd1f6
Michal Jenis <michal.jenis@idnes.cz>
2014-10-14 12:45:25 +0200

    Show singIn form in homepage

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -18,4 +18,13 @@ class HomepagePresenter extends BasePresenter
 		$this->template->anyVariable = 'any value';
 	}
 
+	public function createComponentSignInForm($name)
+	{
+		$form = new \Components\BaseFormComponent(function($parent,$name) {
+							return new \Forms\LogInForm($parent,$name);
+						}, $this, $name);
+		$form->directRender = false;
+		return $form;
+	}
+
 }
