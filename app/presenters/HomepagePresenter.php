b400e7162796bd551960dfa8c3ce0aec935028c5
Michal Jenis <michal.jenis@idnes.cz>
2014-10-15 16:12:17 +0200

    Fix factory name from LogIn form

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -18,7 +18,7 @@ class HomepagePresenter extends BasePresenter
 		$this->template->anyVariable = 'any value';
 	}
 
-	public function createComponentSignInForm($name)
+	public function createComponentLogInForm($name)
 	{
 		$form = new \Components\BaseFormComponent(function($parent,$name) {
 							return new \Forms\LogInForm($parent,$name);
