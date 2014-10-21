950aa9d058fc24ae77f401a9403ce97041fe9eaf
Michal Jenis <michal.jenis@idnes.cz>
2014-10-21 16:54:07 +0200

    Add database to repo

---
+++ database/schema.sql
@@ -0,0 +1,30 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+
+-- -----------------------------------------------------
+-- Table `categories`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `categories` ;
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `parent` INT NULL DEFAULT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  `active` SMALLINT NOT NULL DEFAULT 1,
+  `order` SMALLINT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_unique` (`name` ASC),
+  INDEX `parent_index` (`parent` ASC),
+  CONSTRAINT `fk1`
+    FOREIGN KEY (`parent`)
+    REFERENCES `categories` (`id`)
+    ON DELETE SET NULL
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
