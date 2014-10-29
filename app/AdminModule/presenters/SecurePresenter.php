e6fb8e2341c0df7cff94abdf747311d49a28c233
Michal Jenis <michal.jenis@idnes.cz>
2014-10-29 15:51:44 +0100

    Add Dashboard and Secure presneter into Admin module

---
+++ app/AdminModule/presenters/SecurePresenter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+use Nette;
+
+/**
+ * Presenter for secure requests (need logged user)
+ *
+ * @package App\AdminModule\Presenters
+ * @author Michal Jenis <jenis.michal@gmail.com>
+ */
+class SecurePresenter extends BasePresenter
+{
+	protected function startup()
+	{
+		parent::startup();
+
+		// Check if user is logged in
+		if (!$this->getUser()->isLoggedIn()) {
+			$this->redirect('Login:default');
+		}
+
+	}
+}
