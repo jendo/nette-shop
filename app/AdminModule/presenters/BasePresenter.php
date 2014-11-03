2391d4eeac396e4b10dd0850203fe440c3b63daa
Michal Jenis <michal.jenis@idnes.cz>
2014-11-03 15:44:33 +0100

    Fix admin base presenter - fix parent

--- app/AdminModule/presenters/BasePresenter.php
+++ app/AdminModule/presenters/BasePresenter.php
@@ -5,7 +5,7 @@ namespace App\AdminModule\Presenters;
 /**
  * Base presenter for all admin presenters
  */
-abstract class BasePresenter extends \BasePresenter
+abstract class BasePresenter extends \App\Presenters\BasePresenter
 {
 
 
