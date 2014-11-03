beb6559951ae1d8bb768122e9eaacea0146e2f01
Michal Jenis <michal.jenis@idnes.cz>
2014-11-03 15:57:09 +0100

    Implement form mstg target into js

--- www/js/main.js
+++ www/js/main.js
@@ -1,11 +1,27 @@
 
 "use strict";
 
+Nette.formMsgTarget = Nette.formMsgTarget || {};
+
 Nette.validators.ApplicationFormsCustomValidators_divisibilityValidator = function (elem, arg, value) {
 	return value % arg === 0;
 }
 
+// Override addError function
+Nette.addError = function(elem, message) {
+	if (elem.focus) {
+		elem.focus();
+	}
+	if (message) {
+		if (Nette.formMsgTarget.name.length > 0) {
+			var prefix  = Nette.formMsgTarget.type == 'class' ? '.' : '#';
+			$(prefix + Nette.formMsgTarget.name).html(message).slideDown('fast',function(){});
+		} else {
+			alert(message);
+		}
+	}
+};
 
 $(function(){
-
+	//alert(Nette.formMsgTarget);
 });
