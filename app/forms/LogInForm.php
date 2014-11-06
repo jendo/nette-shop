39e356326bea92e73b1b1602896e0db26195fc1a
Michal Jenis <michal.jenis@idnes.cz>
2014-11-06 17:50:41 +0100

    Final ajax login form

--- app/forms/LogInForm.php
+++ app/forms/LogInForm.php
@@ -3,6 +3,7 @@
 namespace Forms;
 
 use \Application\Forms\CustomValidators;
+use Nette\Application\Responses\JsonResponse;
 
 final class LogInForm extends BaseForm
 {
@@ -25,15 +26,15 @@ final class LogInForm extends BaseForm
 
 		// Text inpout -  email
 		$textControls[] = $this->addText('login', 'Login')
-						->setAttribute('placeholder', 'Login')
-						->addRule(\Nette\Forms\Form::FILLED, 'Prosím vyplňte vyznačené pole.');
+						->setAttribute('placeholder', 'Login');
+						//->addRule(\Nette\Forms\Form::FILLED, 'Prosím vyplňte vyznačené pole.');
 						//Custom validator testing
 						//->addRule($this->getCustomValidtorsClassName() . CustomValidators::IS_DIVISIBLE,'First number must be %d multiple', 2);
 
 		// Text input - password
 		$textControls[] = $this->addPassword('pass', 'Password')
-						->setAttribute('placeholder', 'Password')
-						->addRule(\Nette\Forms\Form::FILLED, 'Prosím vyplňte vyznačené pole.');
+						->setAttribute('placeholder', 'Password');
+						//->addRule(\Nette\Forms\Form::FILLED, 'Prosím vyplňte vyznačené pole.');
 
 		// Submit button
 		$submit = $this->addSubmit('signup', 'Log in');
@@ -54,12 +55,29 @@ final class LogInForm extends BaseForm
 	 */
 	public function formValidate($form)
 	{
+		// Full control for response
+		// This terminates life cycle of presenter
+		//$this->getPresenter()->sendResponse(new JsonResponse(array('asdasd' => 'asdasd')));
 
+		$values = $form->getValues(TRUE);
+
+		// all fields are manfatory
+		foreach ($values as $key => $value) {
+			if (empty($value)) {
+				$this->presenter->payload->target['class'] = $this->getMsgTarget();
+				$this->presenter->payload->focus['name'] = $key;
+				$form->addError('Please fill in all mandatory fields.');
+				$this->presenter->invalidateControl('loginFormSnippet');
+				return false;
+			}
+		}
+
+		return $form->hasErrors();
 	}
 
 	public function processForm($form)
 	{
-		
+
 	}
 
 	public function formSubmitted($form)
