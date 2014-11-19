4bfa2cff0505effe0d17a25aab79b62ba2bced9d
Michal Jenis <michal.jenis@idnes.cz>
2014-11-19 15:30:17 +0100

    Create new routes for categories and products

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -10,6 +10,8 @@ use Nette,
 
 /**
  * Router factory.
+ * Text wrapped in [] in  mask is optional
+ * Text wrapped in <> in mask is variable paramaters passed to presenter
  */
 class RouterFactory
 {
@@ -19,14 +21,45 @@ class RouterFactory
 	 */
 	public function createRouter()
 	{
+		/**
+		 * Object Route
+		 * 1 parameter is mask
+		 * 2 parameter is default action of presenter
+		 * 3 optional parameter is for flags Route::ONE_WAY or Route::SECURED
+		 */
 		$router = new RouteList();
 
-		$router[] = new Route('admin/<presenter>/<action>/<id>', array(
-        'module' => 'Admin',
-        'presenter' => 'Dashboard',
-        'action' => 'default',
-        'id' => NULL,
-    ));
+		// Admin
+		$router[] = new Route(
+						'admin/<presenter>/<action>/<id>',
+						array(
+							'module' => 'Admin',
+							'presenter' => 'Dashboard',
+							'action' => 'default',
+							'id' => NULL
+						)
+		);
+
+		// Category
+		$router[] = new Route(
+						'c/<id>/<name .*>',
+						array(
+							'module' => 'Front',
+							'presenter' => 'Category',
+							'action'	=> 'show'
+						)
+		);
+
+		// Product
+		$router[] = new Route(
+						'p/<id>/<name .*>',
+						array(
+								'module' => 'Front',
+								'presenter' => 'Product',
+								'action' => 'show'
+						)
+		);
+
 
 		$router[] = new Route('<presenter>/<action>[/<id>]', array(
 				'module' => 'Front',
