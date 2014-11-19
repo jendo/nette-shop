3eb3dd1d39d75a0aa4cfe08b049a8f844359a356
Michal Jenis <michal.jenis@idnes.cz>
2014-11-19 16:03:57 +0100

    Catch 404 by bad url for categories

--- app/FrontModule/presenters/CategoryPresenter.php
+++ app/FrontModule/presenters/CategoryPresenter.php
@@ -21,9 +21,24 @@ class CategoryPresenter extends BasePresenter
 	 */
 	public function actionShow($id, $name)
 	{
-		//var_dump(\Nette\Utils\Strings::webalize($name));
 		$categoryManager = $this->getCategoryManager();
 		$this->category = $categoryManager->find($id);
+
+		if (FALSE === $this->category) {
+
+			// Set 404 by set new template
+			//$this->setView('notFound');
+			//return;
+
+			// Ot throw exception for ErrorPresenter
+			$message = '';
+			$code = 404;
+			throw new \Nette\Application\BadRequestException($message, $code);
+		}
+
+		if ($this->category->webname != $name) {
+			$this->redirect(301,'Category:show', array('id' => $id, 'name' => $this->category->webname));
+		}
 
 	}
 
