fcb456811c3cd76ca9ebfd2a59b92255dbd9e41c
Michal Jenis <michal.jenis@idnes.cz>
2014-11-20 17:47:13 +0100

    Add File manager

---
+++ app/models/Core/FileManager.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of FileManager
+ *
+ * @author JenisM01
+ */
+class FileManager
+{
+	//put your code here
+}
+
+?>
