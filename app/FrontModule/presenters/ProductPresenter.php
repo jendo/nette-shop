8f868e370b0ddb9d2fd9c02e50f5b6b162dbf103
Michal Jenis <michal.jenis@idnes.cz>
2014-12-02 15:28:19 +0100

    Create ProductPresenter

---
+++ app/FrontModule/presenters/ProductPresenter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\FrontModule\Presenters;
+
+final class ProductPresenter extends BasePresenter
+{
+
+	/**
+	 *
+	 * @param int $id
+	 * @param string $name
+	 */
+	public function actionShow($id, $name)
+	{
+
+	}
+
+	public function renderShow()
+	{
+
+	}
+
+}
