244610f187782c9102aa378b3630bcd155996f4b
Michal Jenis <michal.jenis@idnes.cz>
2014-12-10 11:16:40 +0100

    Add format price helper

--- app/models/Template/TemplateHelpers.php
+++ app/models/Template/TemplateHelpers.php
@@ -35,17 +35,31 @@ class TemplateHelpers extends \Nette\Object
 		}
 	}
 
-	public function formatPrice($price){
+	/**
+	 * Formt prive with currency
+	 *
+	 * @param float $price
+	 * @return string
+	 */
+	public function formatPrice($price)
+	{
 		$decimals = $this->parameters['price']['decimals'];
 		$decimalpoint = $this->parameters['price']['decimalpoint'];
 		$separator = $this->parameters['price']['separator'];
 		$currency = $this->parameters['price']['currency'];
 
 		$foramttedNumber = number_format($price,$decimals,$decimalpoint,$separator);
-
 		return html_entity_decode($foramttedNumber . '&nbsp;' . $currency);
 	}
 
+	/**
+	 *
+	 * @param \App\Model\File\File $image
+	 * @param \Nette\Application\UI\Presenter $presenter
+	 * @param type $maxwidth
+	 * @param type $maxheight
+	 * @return type
+	 */
 	public function fullImgUrl(\App\Model\File\File $image, \Nette\Application\UI\Presenter $presenter, $maxwidth = 100, $maxheight = 100)
 	{
 		$fakeFilename = md5($image->getFilename()). '.png';
@@ -53,6 +67,15 @@ class TemplateHelpers extends \Nette\Object
 		return $presenter->link(':Core:Tools:showFullImage', $attrs);
 	}
 
+	/**
+	 *
+	 * @param type $id
+	 * @param type $filename
+	 * @param \Nette\Application\UI\Presenter $presenter
+	 * @param type $maxwidth
+	 * @param type $maxheight
+	 * @return type
+	 */
 	public function showFile($id,$filename,\Nette\Application\UI\Presenter $presenter, $maxwidth = 100, $maxheight = 100)
 	{
 		//return 'no_image.jpg';
