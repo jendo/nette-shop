f9ed3441e7113226592405d40e30991c763f1d04
Michal Jenis <jenis.michal@gmail.com>
2014-12-13 14:09:50 +0100

    Change datetime to timestamp and add special price

--- database/views.sql
+++ database/views.sql
@@ -18,7 +18,7 @@ CREATE OR REPLACE VIEW `view_product`
 			f.filename
 	FROM product p
 	INNER JOIN product_category pc ON (pc.product_id = p.id)
-	INNER JOIN  product_file pf ON (p.id = pf.product_id)
+	INNER JOIN  file_product pf ON (p.id = pf.product_id)
 	INNER JOIN file f ON (f.id = pf.file_id)
   WHERE f.main = 1
 	ORDER BY p.name ASC
